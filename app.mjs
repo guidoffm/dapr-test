d287cd03d47799805863318827646696be5293a9
guidoffm <guidoffm@users.noreply.github.com>
2022-04-19 20:43:56 +0200

    Updated app.mjs

--- app.mjs
+++ app.mjs
@@ -4,11 +4,12 @@ import express from 'express';
 const app = express();
 const port = 3000;
 const daprClient = new DaprClient();
-app.get(async (_req, res, next) => {
+app.get('*', async (_req, res, next) => {
     try {
-        const data = await daprClient.secret.getBulk('secretstore');
+        const data = await daprClient.secret.getBulk('configstore');
         console.log(data);
-        res.send(data);
+        res.setHeader('Content-type', 'application/json');
+        res.send(JSON.stringify(data,null,4));
     } catch (err) {
         console.error(err);
         next(err);
